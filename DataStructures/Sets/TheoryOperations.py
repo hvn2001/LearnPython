f8e07dac6fac0275a61a1bf3f083d51d258dc5cd
HoangVu <hoangvu1991uit@gmail.com>
2020-03-18 17:00:39 +0700

    Data Structure : Set

---
+++ DataStructures/Sets/TheoryOperations.py
@@ -0,0 +1,25 @@
+print("--------Union---------")
+set_A = {1, 2, 3, 4}
+set_B = {'a', 'b', 'c', 'd'}
+
+print(set_A | set_B)
+print(set_A.union(set_B))
+print(set_B.union(set_A))
+
+print("--------Intersection---------")
+set_A = {1, 2, 3, 4}
+set_B = {2, 8, 4, 16}
+
+print(set_A & set_B)
+print(set_A.intersection(set_B))
+print(set_B.intersection(set_A))
+
+print("--------Difference---------")
+set_A = {1, 2, 3, 4}
+set_B = {2, 8, 4, 16}
+
+print(set_A - set_B)
+print(set_A.difference(set_B))
+
+print(set_B - set_A)
+print(set_B.difference(set_A))
