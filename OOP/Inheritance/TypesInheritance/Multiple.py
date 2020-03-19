ca62bfdfebb1028090b1336e26d714b1df70fd04
HoangVu <hoangvu1991uit@gmail.com>
2020-03-19 10:41:01 +0700

    Types of Inheritance

---
+++ OOP/Inheritance/TypesInheritance/Multiple.py
@@ -0,0 +1,23 @@
+class CombustionEngine():  # Child class inherited from Engine
+    def setTankCapacity(self, tankCapacity):
+        self.tankCapacity = tankCapacity
+
+
+class ElectricEngine():  # Child class inherited from Engine
+    def setChargeCapacity(self, chargeCapacity):
+        self.chargeCapacity = chargeCapacity
+
+
+# Child class inherited from CombustionEngine and ElectricEngine
+
+
+class HybirdEngine(CombustionEngine, ElectricEngine):
+    def printDetails(self):
+        print("Tank Capacity:", self.tankCapacity)
+        print("Charge Capacity:", self.chargeCapacity)
+
+
+car = HybirdEngine()
+car.setChargeCapacity("250 W")
+car.setTankCapacity("20 Litres")
+car.printDetails()
