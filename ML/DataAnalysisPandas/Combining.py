5cad43f495e1e3331e94d48f6e0460c6d4624d1d
HoangVu <hoangvu1991uit@gmail.com>
2020-03-24 13:40:22 +0700

    pandas: Combining: concatenation, merging: add output

--- ML/DataAnalysisPandas/Combining.py
+++ ML/DataAnalysisPandas/Combining.py
@@ -9,14 +9,34 @@ df2 = pd.DataFrame({'c1': [5, 6], 'c2': [7, 8]},
 df3 = pd.DataFrame({'c1': [5, 6], 'c2': [7, 8]})
 
 concat = pd.concat([df1, df2], axis=1)
-# Newline to separate print statements
 print('{}\n'.format(concat))
+'''
+    c1  c2  c1  c2
+r1   1   3   5   7
+r2   2   4   6   8
+'''
 
 concat = pd.concat([df2, df1, df3])
 print('{}\n'.format(concat))
+'''
+    c1  c2
+r1   5   7
+r2   6   8
+r1   1   3
+r2   2   4
+0    5   7
+1    6   8
+'''
 
 concat = pd.concat([df1, df3], axis=1)
 print('{}\n'.format(concat))
+'''
+     c1   c2   c1   c2
+r1  1.0  3.0  NaN  NaN
+r2  2.0  4.0  NaN  NaN
+0   NaN  NaN  5.0  7.0
+1   NaN  NaN  6.0  8.0
+'''
 
 print('------B. Merging------')
 mlb_df1 = pd.DataFrame({'name': ['john doe', 'al smith', 'sam black', 'john doe'],
@@ -27,10 +47,28 @@ mlb_df2 = pd.DataFrame({'name': ['john doe', 'al smith', 'jack lee'],
                         'rbi': [80, 100, 12]})
 
 print('{}\n'.format(mlb_df1))
+'''
+        name pos  year
+0   john doe  1B  2000
+1   al smith   C  2004
+2  sam black   P  2008
+3   john doe  2B  2003
+'''
 print('{}\n'.format(mlb_df2))
+'''
+       name  rbi  year
+0  john doe   80  2000
+1  al smith  100  2004
+2  jack lee   12  2012
+'''
 
 mlb_merged = pd.merge(mlb_df1, mlb_df2)
 print('{}\n'.format(mlb_merged))
+'''
+       name pos  year  rbi
+0  john doe  1B  2000   80
+1  al smith   C  2004  100
+'''
 
 print('------Ex: ------')
 
