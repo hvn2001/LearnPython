0f3546a301b2350bb432767c9bf5317075cb2cc7
HoangVu <hoangvu1991uit@gmail.com>
2020-03-24 23:02:28 +0700

    Data Preprocessing with scikit-learn: Labeled Data

---
+++ ML/DataPreprocessingScikitLearn/LabeledData.py
@@ -0,0 +1,113 @@
+from sklearn.datasets import load_breast_cancer
+
+print('------A. Class labels------')
+bc = load_breast_cancer()
+print('{}\n'.format(repr(bc.data)))
+print('Data shape: {}\n'.format(bc.data.shape))
+
+# Class labels
+print('{}\n'.format(repr(bc.target)))
+print('Labels shape: {}\n'.format(bc.target.shape))
+
+# Label names
+print('{}\n'.format(list(bc.target_names)))
+
+malignant = bc.data[bc.target == 0]
+print('Malignant shape: {}\n'.format(malignant.shape))
+
+benign = bc.data[bc.target == 1]
+print('Benign shape: {}\n'.format(benign.shape))
+'''
+array([[1.799e+01, 1.038e+01, 1.228e+02, ..., 2.654e-01, 4.601e-01,
+        1.189e-01],
+       [2.057e+01, 1.777e+01, 1.329e+02, ..., 1.860e-01, 2.750e-01,
+        8.902e-02],
+       [1.969e+01, 2.125e+01, 1.300e+02, ..., 2.430e-01, 3.613e-01,
+        8.758e-02],
+       ...,
+       [1.660e+01, 2.808e+01, 1.083e+02, ..., 1.418e-01, 2.218e-01,
+        7.820e-02],
+       [2.060e+01, 2.933e+01, 1.401e+02, ..., 2.650e-01, 4.087e-01,
+        1.240e-01],
+       [7.760e+00, 2.454e+01, 4.792e+01, ..., 0.000e+00, 2.871e-01,
+        7.039e-02]])
+
+Data shape: (569, 30)
+
+array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+       0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0,
+       1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0,
+       1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1,
+       1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0,
+       0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1,
+       1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1,
+       1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0,
+       0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0,
+       1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1,
+       1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1,
+       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1,
+       1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0,
+       0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+       0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0,
+       1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1,
+       1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0,
+       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1,
+       1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+       1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1,
+       1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1,
+       1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+       1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1])
+
+Labels shape: (569,)
+
+['malignant', 'benign']
+
+Malignant shape: (212, 30)
+
+Benign shape: (357, 30)
+'''
+
+print('------Ex: ------')
+
+
+def get_label_info(component_data, labels,
+                   class_label, label_names):
+    label_name = label_names[class_label]
+    label_data = component_data[labels == class_label]
+    return (label_name, label_data)
+
+
+def separate_data(component_data, labels,
+                  label_names):
+    separated_data = []
+    for class_label in range(len(label_names)):
+        separated_data.append(get_label_info(
+            component_data, labels, class_label, label_names))
+    return separated_data
+
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.decomposition import PCA
+
+bc = load_breast_cancer()
+pca_obj = PCA(n_components=2)
+component_data = pca_obj.fit_transform(bc.data)
+labels = bc.target
+label_names = bc.target_names
+# Using the completed separate_data function
+separated_data = separate_data(component_data,
+                               labels, label_names)
+
+# Plotting the data
+import matplotlib.pyplot as plt
+
+for label_name, label_data in separated_data:
+    col1 = label_data[:, 0]  # 1st column (1st pr. comp.)
+    col2 = label_data[:, 1]  # 2nd column (2nd pr. comp.)
+    plt.scatter(col1, col2, label=label_name)  # scatterplot
+plt.legend()  # adds legend to plot
+plt.title('Breast Cancer Dataset PCA Plot')
+plt.show()
