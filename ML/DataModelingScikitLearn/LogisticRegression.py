2f9b8881909e0b6a2963cb79e725d310176f9d49
HoangVu <hoangvu1991uit@gmail.com>
2020-03-25 11:10:01 +0700

    Data Modeling with scikit-learn: Bayesian Regression

---
+++ ML/DataModelingScikitLearn/LogisticRegression.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import numpy as np
+
+print('------A. Classification------')
+data = pd.DataFrame([[2100, 800],
+                     [2500, 850],
+                     [1800, 760],
+                     [2000, 800],
+                     [2300, 810]],
+                    )
+labels = pd.DataFrame([10.99, 12.5, 9.99, 10.99, 11.99])
+
+print('Data shape: {}\n'.format(data.shape))
+# Binary labels
+print('Labels:\n{}\n'.format(repr(labels)))
+
+from sklearn import linear_model
+
+reg = linear_model.LogisticRegression()
+reg.fit(data, labels)
+
+new_data = np.array([
+    [0.3, 0.5, -1.2, 1.4],
+    [-1.3, 1.8, -0.6, -8.2]])
+print('Prediction classes: {}\n'.format(
+    repr(reg.predict(new_data))))
+
+''' Educa
+Data shape: (569, 4)
+
+Labels:
+array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+       0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0,
+       1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0,
+       1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1,
+       1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0,
+       0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1,
+       1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1,
+       1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0,
+       0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0,
+       1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1,
+       1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1,
+       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1,
+       1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0,
+       0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+       0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0,
+       1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1,
+       1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0,
+       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1,
+       1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+       1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1,
+       1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1,
+       1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+       1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1])
+
+Prediction classes: array([1, 0])
+'''
+
+print('------A. we choose ''lbfgs''------')
+# predefined dataset
+print('Data shape: {}\n'.format(data.shape))
+# Multiclass labels
+print('Labels:\n{}\n'.format(repr(labels)))
+
+from sklearn import linear_model
+
+reg = linear_model.LogisticRegression(
+    solver='lbfgs',
+    multi_class='multinomial')
+reg.fit(data, labels)
+
+new_data = np.array([
+    [1.8, -0.5, 6.2, 1.4],
+    [3.3, 0.8, 0.1, 2.5]])
+print('Prediction classes: {}\n'.format(
+    repr(reg.predict(new_data))))
+
+''' Educa
+Data shape: (150, 4)
+
+Labels:
+array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+       2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+       2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
+
+Prediction classes: array([2, 0])
+'''
+print('------B. Solvers------')
+from sklearn import linear_model
+
+reg = linear_model.LogisticRegression(
+    solver='lbfgs', max_iter=1000)
+
+print('------C. Cross-validated model------')
+from sklearn import linear_model
+
+reg = linear_model.LogisticRegressionCV(
+    solver='multinomial', max_iter=1000)
+
+print('------Ex: ------')
+
+
+def multiclass_lr(data, labels, max_iter):
+    reg = linear_model.LogisticRegression(solver='lbfgs',
+                                          max_iter=max_iter, multi_class='multinomial')
+    reg.fit(data, labels)
+    return reg
