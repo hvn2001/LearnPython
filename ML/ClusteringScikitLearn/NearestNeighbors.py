2b47b2ab48c30c52471b57d4997ef9dbf4ee9868
HoangVu <hoangvu1991uit@gmail.com>
2020-03-25 13:56:25 +0700

    Clustering with scikit-learn: Nearest Neighbors

---
+++ ML/ClusteringScikitLearn/NearestNeighbors.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+print('------A. Finding the nearest neighbors------')
+
+data = np.array([
+    [5.1, 3.5, 1.4, 0.2],
+    [4.9, 3., 1.4, 0.2],
+    [4.7, 3.2, 1.3, 0.2],
+    [4.6, 3.1, 1.5, 0.2],
+    [5., 3.6, 1.4, 0.2],
+    [5.4, 3.9, 1.7, 0.4],
+    [4.6, 3.4, 1.4, 0.3],
+    [5., 3.4, 1.5, 0.2],
+    [4.4, 2.9, 1.4, 0.2],
+    [4.9, 3.1, 1.5, 0.1]])
+
+from sklearn.neighbors import NearestNeighbors
+
+nbrs = NearestNeighbors()
+nbrs.fit(data)
+new_obs = np.array([[5., 3.5, 1.6, 0.3]])
+dists, knbrs = nbrs.kneighbors(new_obs)
+
+# nearest neighbors indexes
+print('{}\n'.format(repr(knbrs)))
+# nearest neighbor distances
+print('{}\n'.format(repr(dists)))
+
+only_nbrs = nbrs.kneighbors(new_obs,
+                            return_distance=False)
+print('{}\n'.format(repr(only_nbrs)))
+
+print('------A. n_neighbors ------')
+
+data = np.array([
+    [5.1, 3.5, 1.4, 0.2],
+    [4.9, 3., 1.4, 0.2],
+    [4.7, 3.2, 1.3, 0.2],
+    [4.6, 3.1, 1.5, 0.2],
+    [5., 3.6, 1.4, 0.2],
+    [5.4, 3.9, 1.7, 0.4],
+    [4.6, 3.4, 1.4, 0.3],
+    [5., 3.4, 1.5, 0.2],
+    [4.4, 2.9, 1.4, 0.2],
+    [4.9, 3.1, 1.5, 0.1]])
+
+from sklearn.neighbors import NearestNeighbors
+
+nbrs = NearestNeighbors(n_neighbors=2)
+nbrs.fit(data)
+new_obs = np.array([
+    [5., 3.5, 1.6, 0.3],
+    [4.8, 3.2, 1.5, 0.1]])
+dists, knbrs = nbrs.kneighbors(new_obs)
+
+# nearest neighbors indexes
+print('{}\n'.format(repr(knbrs)))
+# nearest neighbor distances
+print('{}\n'.format(repr(dists)))
