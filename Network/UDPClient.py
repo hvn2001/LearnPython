035ae275885c530d5899da52099775aa5ab70001
HoangVu <hoangvu1991uit@gmail.com>
2020-03-26 22:08:50 +0700

    [Network] Writing a UDP Client Program: Reading Data

--- Network/UDPClient.py
+++ Network/UDPClient.py
@@ -1,4 +1,5 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-print('The OS assigned the address {} to me'.format(s.getsockname()))
+message = input('Input lowercase sentence:')
+data = message.encode('ascii')
