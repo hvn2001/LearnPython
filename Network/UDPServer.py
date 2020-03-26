545d913f18587cc4168a5068542f1fe5e86a81b1
HoangVu <hoangvu1991uit@gmail.com>
2020-03-26 21:59:04 +0700

    [Network] UDP IPV4 Socket: Printing the Client’s Message & Encoding

--- Network/UDPServer.py
+++ Network/UDPServer.py
@@ -9,6 +9,8 @@ hostname = '127.0.0.1'
 s.bind((hostname, port))  # Binding the socket to a port and IP address
 print('Listening at {}'.format(s.getsockname()))  # Printing the IP address and port of socket
 while True:
-    data, address = s.recvfrom(MAX_SIZE_BYTES)  # Receive at most 65535 bytes at once
+    data, clientAddress = s.recvfrom(MAX_SIZE_BYTES)  # Receive at most 65535 bytes at once
     message = data.decode('ascii')
     upperCaseMessage = message.upper()
+    print('The client at {} says {!r}'.format(clientAddress, message))
+    data = upperCaseMessage.encode('ascii')
