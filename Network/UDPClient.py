72ce450e4f2d6ce7b1afa46371e14f26d611ca96
HoangVu <hoangvu1991uit@gmail.com>
2020-03-26 22:10:51 +0700

    [Network] Writing a UDP Client Program: Sending It to the Server

--- Network/UDPClient.py
+++ Network/UDPClient.py
@@ -3,3 +3,5 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 message = input('Input lowercase sentence:')
 data = message.encode('ascii')
+s.sendto(data, ('127.0.0.1', 3000))
+print('The OS assigned the address {} to me'.format(s.getsockname()))
