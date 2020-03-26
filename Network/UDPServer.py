7126d38f347a775803e55e5da23cd6290a7fd379
HoangVu <hoangvu1991uit@gmail.com>
2020-03-26 21:54:39 +0700

    [Network] UDP IPV4 Socket: Receiving Messages from Clients

--- Network/UDPServer.py
+++ Network/UDPServer.py
@@ -1,11 +1,12 @@
 import socket
 
+MAX_SIZE_BYTES = 65535  # Mazimum size of a UDP datagram
+
 # Setting up a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = 3000
 hostname = '127.0.0.1'
 s.bind((hostname, port))  # Binding the socket to a port and IP address
 print('Listening at {}'.format(s.getsockname()))  # Printing the IP address and port of socket
-
 while True:
-    # The code to handle clients will go here
+    data, address = s.recvfrom(MAX_SIZE_BYTES)  # Receive at most 65535 bytes at once
