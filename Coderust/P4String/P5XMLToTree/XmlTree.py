80ed880c69a89a8d28ab93fc8628549278b37a38
HoangVu <hoangvu1991uit@gmail.com>
2020-03-28 00:12:27 +0700

    [CodeRust] [String] XML to Tree - Sol

--- Coderust/P4String/P5XMLToTree/XmlTree.py
+++ Coderust/P4String/P5XMLToTree/XmlTree.py
@@ -1,3 +1,56 @@
+class XmlElementType():
+    ELEMENT_UNKNOWN = 1
+    ELEMENT_OPENING_TAG = 2
+    ELEMENT_CLOSING_TAG = 3
+    ELEMENT_TEXT = 4
+
+
+# trim spaces from both sides
+def trim(st):
+    print(st.strip())
+
+
+class XmlElement:
+    def __init__(self):
+        self.element_type = XmlElementType.ELEMENT_UNKNOWN
+        self.node_name = ""
+
+
+class XmlTokenizer:
+    def __init__(self, xml_str):
+        self.xml = xml_str
+        self.current_index = 0
+
+    def get_next_element(self, element):
+        i = self.xml.find("<", self.current_index)
+
+        if i == -1:
+            return False
+
+        temp = self.xml[self.current_index: i]
+        temp = temp.strip()
+
+        if len(temp) != 0:
+            element.node_name = temp
+            element.element_type = XmlElementType.ELEMENT_TEXT
+
+            self.current_index = i
+            return True
+
+        j = self.xml.find(">", i)
+
+        if self.xml[i + 1] == '/':
+            element.node_name = self.xml[i + 2: j]
+            element.element_type = XmlElementType.ELEMENT_CLOSING_TAG
+        else:
+
+            element.node_name = self.xml[i + 1: j]
+            element.element_type = XmlElementType.ELEMENT_OPENING_TAG
+
+        self.current_index = j + 1
+        return True
+
+
 class Node:
     def __init__(self, name):
         self.node_name = name
@@ -5,5 +58,46 @@ class Node:
 
 
 def create_xml_tree(xml):
-    root = Node("Hello")
+    tok = XmlTokenizer(xml)
+    element = XmlElement()
+
+    if tok.get_next_element(element) == False:
+        return None
+
+    st = []
+    root = Node(element.node_name)
+    st.append(root)
+
+    while tok.get_next_element(element) == True:
+
+        n = None
+        if (element.element_type == XmlElementType.ELEMENT_OPENING_TAG or
+                element.element_type == XmlElementType.ELEMENT_TEXT):
+            n = Node(element.node_name)
+            st[-1].children.append(n)
+
+        if element.element_type == XmlElementType.ELEMENT_OPENING_TAG:
+            st.append(n)
+        elif element.element_type == XmlElementType.ELEMENT_CLOSING_TAG:
+            st.pop()
+
     return root
+
+
+def print_tree(root, depth):
+    if root == None:
+        return
+
+    for x in range(0, depth):
+        print(end="\t")
+
+    print(root.node_name)
+
+    for x in range(0, len(root.children)):
+        print_tree(root.children[x], depth + 1)
+
+
+xml = "<xml><data>hello world     </data>    <a><b></b><b><c></c></b></a></xml>"
+head = create_xml_tree(xml)
+
+print_tree(head, 0)
